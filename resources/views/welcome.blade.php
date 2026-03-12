a7937d64731b3b0c0a883f113790e753cf2452bf
Jonathan Peguit Jr. <jonathanjrhayo@gmail.com>
2026-03-12 08:52:42 +0800

    style: enhanced view layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -23,32 +24,45 @@
     <div class="container-fluid d-flex justify-content-center my-5">
         <h1 class="display-6 fw-bold">Book Borrow Log</h1>
     </div>
-
-    <div class="row justify-content-center m-5">
-        <a href="{{ route('create') }}" class="btn btn-primary">Borrow a Book</a>
+    <div class="container p-5">
+        <div class="row justify-content-between m-5">
+        <h2 class="fs-2 fw-semibold p-0 m-0 col-1">Logs</h2>
+        <a href="{{ route('create') }}" class="btn btn-primary col-2"><i class="bi bi-plus"></i>Add Log</a>
     </div>
 
     <div class="row justify-content-center m-5">
         @foreach ($borrowed_records as $record)
-        <div class="card mt-3 p-5">
-            <p class="display-6 fw-bold">{{$record->name}}</p>
-            <p class="display-7">Book Title: {{$record->bookTitle}}</p>
-            @if($record->status=="pending")
-            <div class="badge bg-warning fw-bold w-20 my-3">{{$record->status}}</div>
-            @else
-            <div class="badge bg-success fw-bold w-20 my-3">{{$record->status}}</div>
-            @endif
-            <div class="my-3 flex d-flex card-text  gap-3">
-                <a href="{{route('edit', $record->id)}}" class="btn btn-warning w-60">Edit</a>
-                <form class="w-60" action="{{ route('delete',$record->id) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger w-100">Delete</button>
-                </form>
+        <div class="card m-3 p-3 row">
+            <div class="d-flex align-items-center justify-content-between">
+                <div>
+                    <p><span class="fw-semibold">Book Title: </span><span class="fs-6 fw-bold">{{ $record->bookTitle }}</span></p>
+                    <p><span class="fw-semibold">Borrowed by:</span> {{ $record->name }}</p>
+                    <p><span class="fw-semibold">Borrow time:</span> {{ $record->created_at->format('M j, Y g:i A') }}</p>
+                </div>
+
+                <div class="d-flex align-items-center gap-3">
+                    @if($record->status=="pending")
+                    <div class="badge bg-warning fw-bold w-20 my-3">{{$record->status}}</div>
+                    @else
+                    <div class="badge bg-success fw-bold w-20 my-3">{{$record->status}}</div>
+                    @endif
+                    <div class="my-3 flex d-flex card-text  gap-3">
+                        <a href="{{route('edit', $record->id)}}" class="btn btn-warning w-30">Edit</a>
+                        <form class="w-60" action="{{ route('delete',$record->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger w-30">Delete</button>
+                        </form>
+                    </div>
+                </div>
+
             </div>
+
         </div>
         @endforeach
     </div>
+    </div>
+    
 </body>
 
 </html>
